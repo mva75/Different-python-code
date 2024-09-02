efecb5151c460f6d8e24e86c1ec0a8aa56ec53c2
mva75 <mvafromsib@yahoo.com>
2024-09-02 20:53:06 +0500

    New file python_repos.svg

--- API/python_repos.py
+++ API/python_repos.py
@@ -1,4 +1,7 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
 # Создание вызова API и сохранение ответа.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
@@ -9,10 +12,15 @@ print("Total repositories:", response_dict['total_count'])
 
 # Анализ информации о репозиториях.
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-# Анализ первого репозитория.
-repo_dict = repo_dicts[0]
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+# Построение визуализации.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
